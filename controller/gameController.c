dc0c10b5bd327386eabf666771ee4573761efe63
Caio Baracat <caio.baracat@picpay.com>
2020-09-23 23:43:55 -0300

    adiciona controller

---
+++ controller/gameController.c
@@ -0,0 +1,11 @@
+//
+// Created by Caio Baracat on 23/09/20.
+//
+
+#include "gameController.h"
+
+
+void newGame()
+{
+
+}
---
+++ controller/gameController.h
@@ -0,0 +1,11 @@
+//
+// Created by Caio Baracat on 23/09/20.
+//
+
+#ifndef DOMINO_GAMECONTROLLER_H
+#define DOMINO_GAMECONTROLLER_H
+
+#endif //DOMINO_GAMECONTROLLER_H
+
+
+void newGame();
