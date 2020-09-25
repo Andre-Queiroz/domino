82d422cf2f258a7074fecbaf275bacdb563c3dd5
André Torres de Queiroz <andretorresdequeiroz@gmail.com>
2020-09-24 21:29:03 -0300

    adiciona switch e do while na main

--- main.c
+++ main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include "gameController.h"
+#include "gameView.h"
 
-int main(){
-    newGame();
+int main() {
+    int option;
+
+    do {
+        option = showMainMenu();
+
+        switch (option){
+            case 1:
+                printf("Iniciando novo jogo...\n");
+                newGame();
+                break;
+            case 2:
+                printf("Continuando\n");
+                //call the function
+                break;
+            case 3:
+                printf("Salvando jogo...\n");
+                //call the function
+                break;
+            case 4:
+                printf("Carregando jogo...\n");
+                //call the function
+                break;
+            case 5:
+                printf("Regras do jogo:\n");
+                //call the function
+                break;
+            case 6:
+                printf("Voce saiu do jogo.\n");
+                break;
+        }
+    } while (option != 6);
     return 0;
 }
 
