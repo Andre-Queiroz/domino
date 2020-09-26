653e7b432572db0459e450d4b14e42fb989ec2ac
André Torres de Queiroz <andretorresdequeiroz@gmail.com>
2020-09-26 00:53:07 -0300

    Adicionei opcao de comprar peças

--- controller/gameController.c
+++ controller/gameController.c
@@ -10,8 +10,12 @@ void newGame() {
     Bank bank;
     game.bank = bank;
 
+    Player *p1 = &(game.player1);
+    Player *p2 = &(game.player2);
+
     fillPieces(&game);
     setPlayers(&game);
-
+    drawPieces(7, &game, p1);
+    drawPieces(7, &game, p2);
     gameStart(&game);
 }
