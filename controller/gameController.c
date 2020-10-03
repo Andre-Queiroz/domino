b5bac498f52c0df358031abe225bc577f69b4791
Caio Baracat <caio.baracat@picpay.com>
2020-10-03 01:15:23 -0300

    adiciona rotinas de novo jogo

--- controller/gameController.c
+++ controller/gameController.c
@@ -8,8 +8,20 @@ Game game;
 
 void newGame() {
     Board board;
+    board.total = 0;
+
+    History history;
+    history.total = 0;
+
+    board.history = history;
     game.board = board;
 
     fill(&game);
-    setPlayers(*game);
+    setPlayers(&game);
+
+    draw(&game, 1, 7);
+    draw(&game, 2, 7);
+
+    printf("oi?\n");
+    //startGame(&game);
 }
