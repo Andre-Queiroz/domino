3fba2f762e35532af308e9bbaeccf3f3f72d7391
Caio Baracat <caio.baracat@picpay.com>
2020-10-08 21:21:21 -0300

    WIP subindo

--- main.c
+++ main.c
@@ -4,34 +4,36 @@
 int main() {
     int option;
 
-    do {
-        option = showMainMenu();
+    newGame();
 
-        switch (option){
-            case 1:
-                printf("Iniciando novo jogo...\n");
-                newGame();
-                break;
-            case 2:
-                printf("Continuando\n");
-                //call the function
-                break;
-            case 3:
-                printf("Salvando jogo...\n");
-                //call the function
-                break;
-            case 4:
-                printf("Carregando jogo...\n");
-                //call the function
-                break;
-            case 5:
-                showRules();
-                break;
-            case 6:
-                printf("Voce saiu do jogo.\n");
-                break;
-        }
-    } while (option != 6);
+//    do {
+//        option = showMainMenu();
+//
+//        switch (option){
+//            case 1:
+//                printf("Iniciando novo jogo...\n");
+//                newGame();
+//                break;
+//            case 2:
+//                printf("Continuando\n");
+//                //call the function
+//                break;
+//            case 3:
+//                printf("Salvando jogo...\n");
+//                //call the function
+//                break;
+//            case 4:
+//                printf("Carregando jogo...\n");
+//                //call the function
+//                break;
+//            case 5:
+//                showRules();
+//                break;
+//            case 6:
+//                printf("Voce saiu do jogo.\n");
+//                break;
+//        }
+//    } while (option != 6);
     return 0;
 }
 
