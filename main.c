a12c0eb1d79515ac75de8f0065f03fd0695d176c
Caio Baracat <caio.baracat@picpay.com>
2020-10-27 21:02:28 -0300

    return menu

--- main.c
+++ main.c
@@ -2,38 +2,37 @@
 #include "gameController.h"
 
 int main() {
-    //int option;
+    int option;
 
-    newGame();
+    do {
+        option = showMainMenu();
 
-//    do {
-//        option = showMainMenu();
-//
-//        switch (option){
-//            case 1:
-//                printf("Iniciando novo jogo...\n");
-//                newGame();
-//                break;
-//            case 2:
-//                printf("Continuando\n");
-//                //call the function
-//                break;
-//            case 3:
-//                printf("Salvando jogo...\n");
-//                //call the function
-//                break;
-//            case 4:
-//                printf("Carregando jogo...\n");
-//                //call the function
-//                break;
-//            case 5:
-//                showRules();
-//                break;
-//            case 6:
-//                printf("Voce saiu do jogo.\n");
-//                break;
-//        }
-//    } while (option != 6);
+        switch (option){
+            case 1:
+                printf("Iniciando novo jogo...\n");
+                newGame();
+                break;
+            case 2:
+                printf("Continuando\n");
+                //call the function
+                break;
+            case 3:
+                printf("Salvando jogo...\n");
+                //call the function
+                break;
+            case 4:
+                printf("Carregando jogo...\n");
+                //call the function
+                break;
+            case 5:
+                showRules();
+                break;
+            case 6:
+                printf("Voce saiu do jogo.\n");
+                break;
+        }
+    } while (option != 6);
+
     return 0;
 }
 
